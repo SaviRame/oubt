2940c46f943f049e9437ab60b5ac08d81b41fdc5
Savitha Muthusamy <jmsavitha@gmail.com>
2026-01-13 23:09:31 -0800

    updated

--- day-12-13/glue/run-athena-glue.py
+++ day-12-13/glue/run-athena-glue.py
@@ -1,4 +1,5 @@
 import time
+
 import boto3
 
 s3 = boto3.client("s3")
@@ -12,14 +13,14 @@ ATHENA_WORKGROUP = "primary"
 ATHENA_OUTPUT = "s3://day11-12-sql-glue/athena-query-results/"
 
 # file execution order
-SETUP_FILES = ["setup/00_raw_tables.sql"]
+SETUP_FILES = ["setup/1-raw-tables.sql"]
 TRANSFORM_FILES = [
-    "transforms/01_taxi_trip_enriched.sql",
-    "transforms/02_daily_pickup_zone_metrics.sql",
+    "transforms/1-taxi-trip-enriched.sql",
+    "transforms/2_daily_pickup_zone_metrics.sql",
 ]
 TEST_FILES = [
-    "tests/01_test_taxi_trip_enriched.sql",
-    "tests/02_test_daily_pickup_zone_metrics.sql",
+    "tests/1_test_taxi_trip_enriched.sql",
+    "tests/2_test_daily_pickup_zone_metrics.sql",
 ]
 
 REPAIR_TABLES = [
